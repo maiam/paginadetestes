dbb3d50d6ec0dd38152c7977d737d37232bcff66
maiam <pedrosergiomaia@gmail.com>
2026-03-04 12:09:35 -0300

    correcao de bugs

--- web/App.tsx
+++ web/App.tsx
@@ -28,7 +28,7 @@ const userNavigation = [
   { name: "Sign out", href: "#" },
 ];
 
-function classNames(...classes) {
+function classNames(...classes: (string | undefined | false)[]): string {
   return classes.filter(Boolean).join(" ");
 }
 
